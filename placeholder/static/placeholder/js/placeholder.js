ae3a057bafc82766a8eefad3cc3fcc8afe229edc
Paulo R. Macedo Chaves <proberto.macedo@gmail.com>
2013-06-08 16:27:05 -0300

    Adds placeholders for objects and instances

--- placeholder/static/placeholder/js/placeholder.js
+++ placeholder/static/placeholder/js/placeholder.js
@@ -1,4 +1,6 @@
-var PLACEHOLDER_NODE_DATA_PREFIX = "django:placeholder:"
+var PLACEHOLDER_TEXT_NODE_DATA_PREFIX = "placeholder:text:";
+var PLACEHOLDER_INSTANCE_NODE_DATA_PREFIX = "placeholder:instance:meta:";
+var PLACEHOLDER_OBJECTS_NODE_DATA_PREFIX = "placeholder:objects:meta:";
 
 function list_node_comments(node, comments, recursive) {
     if (comments == null)
@@ -21,21 +23,48 @@ function list_node_comments(node, comments, recursive) {
     }
 }
 
-function is_placeholder(node) {
-    return ((node.nodeType == document.COMMENT_NODE) && (node.data.search(PLACEHOLDER_NODE_DATA_PREFIX) == 0))
+function is_placeholder_text(node) {
+    return (
+        (node.nodeType == document.COMMENT_NODE) &&
+        (node.data.search(PLACEHOLDER_TEXT_NODE_DATA_PREFIX) == 0))
 }
 
-function list_placeholder(element, recursive) {
+function is_placeholder_instance(node) {
+    return (
+        (node.nodeType == document.COMMENT_NODE) &&
+        (node.data.search(PLACEHOLDER_INSTANCE_NODE_DATA_PREFIX) == 0))
+}
+
+
+function is_placeholder_objects(node) {
+    return (
+        (node.nodeType == document.COMMENT_NODE) &&
+        (node.data.search(PLACEHOLDER_OBJECTS_NODE_DATA_PREFIX) == 0))
+}
+
+function list_placeholder_text(element, recursive) {
     return jQuery.grep(list_node_comments(element, [], recursive), function (node, index) {
-        return is_placeholder(node)
+        return is_placeholder_text(node)
+    })
+}
+
+function list_placeholder_instance(element, recursive) {
+    return jQuery.grep(list_node_comments(element, [], recursive), function (node, index) {
+        return is_placeholder_instance(node)
+    })
+}
+
+function list_placeholder_objects(element, recursive) {
+    return jQuery.grep(list_node_comments(element, [], recursive), function (node, index) {
+        return is_placeholder_objects(node)
     })
 }
 
 (function ($) {
     $(function () {
         var placeholders_init = function () {
-            $.each(list_placeholder(document.body), function () {
-                var len = PLACEHOLDER_NODE_DATA_PREFIX.length
+            $.each(list_placeholder_text(document.body), function () {
+                var len = PLACEHOLDER_TEXT_NODE_DATA_PREFIX.length
                 var ph_node = this
                 var meta = jQuery.parseJSON(this.data.slice(len))
                 var $this = $(this.parentNode)
@@ -64,15 +93,78 @@ function list_placeholder(element, recursive) {
                     change()
                 })
             })
+
+            $.each(list_placeholder_instance(document.body), function () {
+                var len = PLACEHOLDER_INSTANCE_NODE_DATA_PREFIX.length
+                var ph_node = this
+                var meta = jQuery.parseJSON(this.data.slice(len))
+                var nodeinstancedata = this.previousSibling.data;
+                var $this = $(this)
+                var $element = $this.next();
+                var $button = $('<a title="Placeholder" class="fancybox placeholder" data-fancybox-type="iframe">✎</a>');
+                $button.attr({
+                    'href': '/admin/' + meta.app_label + "/" + meta.model_name.toLowerCase() + "/" + meta.model_pk + '/?_popup=1'
+                })
+                var offset = $element.offset();
+                $button.css(offset);
+                $button.appendTo(document.body);
+                $button.fancybox({
+                    afterClose: function () {
+                        $.get(location.href, function (source) {
+//                            $element.remove();
+                            source = source.split("<!--" + nodeinstancedata + "-->")[1];
+                            source = source.split("<!--/" + nodeinstancedata + "-->")[0];
+                            $element.remove();
+                            $this.next().remove();
+                            $this.after(source);
+                            $this.remove();
+                        });
+//                        alert(666);
+                    }
+//                    helpers: {title: {type: 'over'}}
+                });
+            })
+
+            $.each(list_placeholder_objects(document.body), function () {
+                var len = PLACEHOLDER_OBJECTS_NODE_DATA_PREFIX.length
+                var ph_node = this
+                var nodeobjectsdata = this.data;
+                var meta = jQuery.parseJSON(this.data.slice(len))
+                var $this = $(this)
+                var $element = $this.next();
+                var $button = $('<a title="Placeholder" class="fancybox placeholder" data-fancybox-type="iframe">✎</a>');
+                $button.attr({
+                    'href': '/admin/' + meta.app_label + "/" + meta.model_name.toLowerCase() + '/?pop=1'
+                });
+                var offset = $element.offset();
+                $button.css(offset);
+                $button.appendTo(document.body);
+                $button.fancybox({
+                    afterClose: function () {
+                        $.get(location.href, function (source) {
+//                            $element.remove();
+                            source = source.split("<!--" + nodeobjectsdata + "-->")[1];
+                            source = source.split("<!--/placeholder:objects-->")[0];
+                            $element.remove();
+                            $this.next().remove();
+                            $this.after(source);
+                            $this.remove();
+                        });
+//                        alert(666);
+                    }
+//                    helpers: {title: {type: 'over'}}
+                });
+            })
+//            $(".fancybox").fancybox();
         }
 
         $.getScript("/static/placeholder/js/jquery.hotkeys.js", function () {
-            $(document).bind('keyup', 'ctrl+e', function(){
-                if (confirm("Carregar edição de conteúdo ?"))
-                {
+            $(document).bind('keyup', 'ctrl+shift+x', function(){
+//                if (confirm("Carregar edição de conteúdo ?"))
+//                {
 //                    var credentials = prompt("Usuário/Senha");
                     placeholders_init();
-                }
+//                }
             });
         })
 
--- placeholder/templatetags/placeholder_tags.py
+++ placeholder/templatetags/placeholder_tags.py
@@ -11,7 +11,9 @@ from simplejson import dumps
 
 register = template.Library()
 
+
 class Placeholder(Tag):
+    name = 'placeholder'
     options = Options(
         Argument('model_object', required=True),
         Argument('model_attribute', required=True, resolve=False)
@@ -19,16 +21,68 @@ class Placeholder(Tag):
 
     def render_tag(self, context, model_object, model_attribute):
         value = getattr(model_object, model_attribute)
-        #my_model = get_model(my_instance._meta.app_label, my_instance.__class__.__name__)
         meta = dumps({
             'app_label': model_object._meta.app_label,
             'model_name': model_object.__class__.__name__,
             'model_attribute': model_attribute,
             'model_pk': model_object.pk
         })
-        value = u"<!--django:placeholder:%s-->%s<!--/django:placeholder-->" % \
+        value = u"<!--placeholder:text:%s-->%s<!--/placeholder:text-->" % \
             (meta, value)
         return value
 
 
 register.tag(Placeholder)
+
+
+class PlaceholderInstance(Tag):
+    name = 'placeholder_instance'
+    options = Options(
+        Argument('model_object', required=True),
+        blocks=[('endplaceholder_instance', 'nodelist', )],
+    )
+
+    def render_tag(self, context, model_object, nodelist):
+        meta = dumps({
+            'app_label': model_object._meta.app_label,
+            'model_name': model_object.__class__.__name__,
+            'model_pk': model_object.pk
+        })
+        output = nodelist.render(context)
+        output = \
+            (
+                u"<!--placeholder:instance:{model_name}:{pk}-->"  \
+                u"<!--placeholder:instance:meta:{meta}-->" \
+                u"{output}" \
+                u"<!--/placeholder:instance:meta-->" \
+                u"<!--/placeholder:instance:{model_name}:{pk}-->").format(
+                    pk=model_object.pk, meta=meta,
+                    model_name=model_object.__class__.__name__, output=output)
+        return output
+
+register.tag(PlaceholderInstance)
+
+
+class PlaceholderObjects(Tag):
+    name = 'placeholder_objects'
+    options = Options(
+        Argument('app_label', required=True),
+        Argument('model_name', required=True),
+        blocks=[('endplaceholder_objects', 'nodelist', )],
+    )
+
+    def render_tag(self, context, app_label, model_name, nodelist):
+        meta = dumps({
+            'app_label': app_label,
+            'model_name': model_name,
+        })
+        output = nodelist.render(context)
+        output = \
+            (
+                u"<!--placeholder:objects:meta:{meta}-->" \
+                u"{output}" \
+                u"<!--/placeholder:objects:meta-->").format(
+                    meta=meta, output=output)
+        return output
+
+register.tag(PlaceholderObjects)
