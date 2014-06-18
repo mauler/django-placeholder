92fa80011dfa04f9299325e3586566782211938b
Paulo R <proberto.macedo@gmail.com>
2014-06-17 22:48:18 -0300

    Adds expire cache to response

--- placeholder/middleware.py
+++ placeholder/middleware.py
@@ -2,6 +2,11 @@ from .utils import expire_page
 
 
 class ExpireCacheMiddleware(object):
+
     def process_request(self, request):
+        if '__placeholder_expire_page' in request.GET:
+            expire_page(request.path)
+
+    def process_response(self, request, response):
         if '__placeholder_expire_page' in request.GET:
             expire_page(request.path)
