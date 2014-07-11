9e76f0f476226815bdf8a4adc344b13dce5b8392
Paulo R <proberto.macedo@gmail.com>
2014-07-11 16:37:38 -0300

    Fixes middleware

--- placeholder/middleware.py
+++ placeholder/middleware.py
@@ -10,3 +10,4 @@ class ExpireCacheMiddleware(object):
     def process_response(self, request, response):
         if '__placeholder_expire_page' in request.GET:
             expire_page(request.path)
+        return response
